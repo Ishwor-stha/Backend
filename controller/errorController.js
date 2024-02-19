5363127f1394d8d3d62f7cffcdee81974d354b1a
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-02-19 09:02:34 +0545

    enhancing error handling

--- controller/errorController.js
+++ controller/errorController.js
@@ -3,6 +3,7 @@ module.exports=(err,req,res,next)=>{
     // all err data will come through errorHandling file from util folder 
     res.status(err.statusCode).json({
         status:err.status,
-        message:err.message
+        message:err.message,
+        detail:err
     })
 }
