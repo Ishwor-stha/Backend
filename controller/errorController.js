f3fe8a7d42f011b11e0f9e0dde6369ed1bb91dc6
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-02-21 10:27:54 +0545

    displaying seperate error in development and production

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,26 +1,35 @@
 // error handling middleware
-const errorInDevelopment=(err,res)=>{
+
+//  function for developer to display  detailed error . (FOR DEVELOPMENT)
+const errorInDevelopment = (err, res) => {
     res.status(err.statusCode).json({
-        status:err.status,
-        message:err.message,
+        status: err.status,
+        message: err.message,
         err,
-        detail:err.stack
-    })
-}
-const errorInProduction=(err,res)=>{
-    res.status(err.statusCode).json({
-        status:err.status,
-        message:err.message,
-      
+        detail: err.stack
     })
 }
 
-module.exports=(err,req,res,next)=>{
+// function for users  to display  normal error. (FOR PRODUCTION)  
+const errorInProduction = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+
+    })
+}
+
+module.exports = (err, req, res, next) => {
     // all err data will come through errorHandling file from util folder 
-    if(process.env.NODE_ENV ==="development"){
-        errorInDevelopment(err,res)
+    // if node environment is in  development 
+    if (process.env.NODE_ENV === "development") {
+        //calling function with  err and res as argument
+        errorInDevelopment(err, res)
     }
-    else if(process.env.NODE_ENV === "production"){
-        errorInProduction(err,res)
+    // If node env is in production 
+    else if (process.env.NODE_ENV === "production") {
+
+        //calling function with  err and res as argument
+        errorInProduction(err, res)
     }
 }
