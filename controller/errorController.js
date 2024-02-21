d2e75d1b13b35c426189d4a97a2b0af9defcdba2
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-02-21 10:11:35 +0545

    dividing the error for production and development

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,9 +1,26 @@
 // error handling middleware
-module.exports=(err,req,res,next)=>{
-    // all err data will come through errorHandling file from util folder 
+const errorInDevelopment=(err,res)=>{
     res.status(err.statusCode).json({
         status:err.status,
         message:err.message,
-        detail:err
+        err,
+        detail:err.stack
     })
+}
+const errorInProduction=(err,res)=>{
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message,
+      
+    })
+}
+
+module.exports=(err,req,res,next)=>{
+    // all err data will come through errorHandling file from util folder 
+    if(process.env.NODE_ENV ==="development"){
+        errorInDevelopment(err,res)
+    }
+    else if(process.env.NODE_ENV === "production"){
+        errorInProduction(err,res)
+    }
 }
