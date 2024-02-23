501d53570b3c4c405fdc1576c0556cc4bab798e7
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-02-23 19:42:23 +0545

    creating a userController

--- controller/userController.js
+++ controller/userController.js
@@ -4,7 +4,17 @@ const errorHandling=require('../util/errorHandling')
 
 module.exports.createUser=async(req,res,next)=>{
     try{
-        const createNewUser=await User.create(req.body)
+        // const createNewUser=await User.create(req.body)
+
+        // It is secure to take input this way by filtering 
+        const createNewUser=await User.create({
+            name:req.body.name,
+            email:req.body.email,
+            // photo is optional
+            photo:req.body.photo,
+            password:req.body.password,
+            passwordConfirm:req.body.passwordConfirm
+        })
         res.status(200).json({
             status:"success",
             userDetail:createNewUser
