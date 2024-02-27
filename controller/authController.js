57ff155bbfaee00edf5737cc2c022fae3f191106
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-02-27 19:39:59 +0545

    protecting routes

--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,7 @@
 const User = require('../modle/userModel')
 const errorHandling = require('../util/errorHandling')
 const jwt = require('jsonwebtoken')
-const bcrypt=require('bcryptjs')
+const bcrypt = require('bcryptjs')
 
 module.exports.createUser = async (req, res, next) => {
     try {
@@ -28,37 +28,54 @@ module.exports.createUser = async (req, res, next) => {
 
 }
 
-module.exports.login =async (req, res, next) => {
-// Taking userName from client side
+module.exports.login = async (req, res, next) => {
+    // Taking userName from client side
     const userName = req.body.name
     // taking password from client side
     const userPassword = req.body.password
     // it there is no user name or password then terminate current middleware and call errorhandling middleware with two argument ie(errormessage,statusCode)
     if (!userName || !userPassword) return next(new errorHandling("Please enter username or password", 400))
-    
-// searching the document of user in DataBase providing username which we have received form clientSide
-    const DbUserDetail=await User.findOne({name:userName})
+
+    // searching the document of user in DataBase providing username which we have received form clientSide
+    const DbUserDetail = await User.findOne({ name: userName })
 
     // if there is no userDetail then terminate current middleware and call errorhandling middleware
-    if(!DbUserDetail) return next(new errorHandling("The username or password is incorrect",401))
-   
+    if (!DbUserDetail) return next(new errorHandling("The username or password is incorrect", 401))
+
     // storing the hashed userpassword of db in variable
-    const dbPassword=DbUserDetail.password
+    const dbPassword = DbUserDetail.password
 
     // comparing the hashed Database password and the password of Client side using bcrypt  
-    const validPassword=await bcrypt.compare(userPassword,dbPassword)//true/false
+    const validPassword = await bcrypt.compare(userPassword, dbPassword)//true/false
 
     // if password doesnot match then terminate this/current middleware and call error handling middleware
-    if(!validPassword)return next(new errorHandling("The username or password is incorrect",401))
+    if (!validPassword) return next(new errorHandling("The username or password is incorrect", 401))
 
     // if password and username is valid then send the response
     res.status(200).json({
-        status:"success",
-        message:"you have loged in"
+        status: "success",
+        message: "you have loged in"
     })
 
 
+
+
+
+}
+
+module.exports.protect =async (req, res, next) => {
+    let token
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        let arrayString = req.headers.authorization.split(" ")
+        token = arrayString[1]
+
+    }
+
+    if (!token) next(new errorHandling("please login to get access", 401))
     
+    const isValid= await jwt.verify(token,process.env.JWT_SECRETKEY)
+    console.log(isValid)
 
 
+    next()
 }
--- routing/tourRoute.js
+++ routing/tourRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const tours = require('../controller/getTour')//importing  the middlewares of tourcontroller 
+const authController=require('../controller/authController')
 const router = express.Router()
 
 
-router.route('/').get(tours.getTours).post(tours.postTour)
+router.route('/').get(authController.protect,tours.getTours).post(tours.postTour)
 router.route('/:id').get(tours.getOnlyOneTour).patch(tours.updateById).delete(tours.deleteById)
 
 module.exports=router
