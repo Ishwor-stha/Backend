342f3695b9965c3a4c5f4b8c490afa8ee53c9e42
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-03-27 15:32:24 +0545

    working on User controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,31 +1,33 @@
-const User = require('../model/userModel');
+const User = require('../modle/userModel');
 const errorHandling=require('../util/errorHandling')
 
 const filteredData = (req, ...fields) => {
     const data = {};
-    const keyValue = Object.keys(req.body);
+
+    const keyValue = Object.keys(req.body)
     keyValue.forEach(el => {
-        fields.forEach(field => {
-            if (el.includes(field)) {
-                data[el] = req.body[el];
-            }
-        });
+        if(fields.includes(el)) data[el]=req.body[el]
     });
+     
     return data;
 };
 
 module.exports.updateProfile = async (req, res, next) => {
     try {
         const userId = req.userId;
-        const data = filteredData(req, 'name', 'email');
+        // console.log(userId)
+        const data = filteredData(req, 'name','email');
+        
         if(!data) return next(new errorHandling("Empty data",404))
         const updateUser = await User.findByIdAndUpdate(userId, data, { new: true, runValidators: true });
 
         if (!updateUser) return next(new errorHandling("User not found",404))
             
 
-        res.status(200).json({ message: "Profile updated successfully",
-         user: updateUser });
+        res.status(200).json({ 
+            message: "Profile updated successfully",
+           
+        });
     } catch (error) {
         next(error);
     }
