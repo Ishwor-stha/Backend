53eef511c0dd21f36590c69cfb29a251e0e18a25
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-03-27 15:39:31 +0545

    working on User controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,37 +1,44 @@
 const User = require('../modle/userModel');
-const errorHandling=require('../util/errorHandling')
+const errorHandling = require('../util/errorHandling')
 
 // @desc: function for filtering the field name 
 const filteredData = (req, ...fields) => {
     const data = {};
 
+    // storing the keyName of req.body object
     const keyValue = Object.keys(req.body)
     keyValue.forEach(el => {
-        if(fields.includes(el)) data[el]=req.body[el]
+        // if a key exists on field array then store the object 
+        if (fields.includes(el)) data[el] = req.body[el]
     });
-     
+
     return data;
 };
 
 
-// method:PATCH
+// @method:PATCH
 // @EndPoint:localhost:3000/api/v1/user/updateProfile
 // @desc : A controller for updating the user detail(ie.name,email) 
 module.exports.updateProfile = async (req, res, next) => {
     try {
+        // userId from authContorller.protect middleWare
         const userId = req.userId;
         // console.log(userId)
-        const data = filteredData(req, 'name','email');
-        
-        if(!data) return next(new errorHandling("Empty data",404))
+
+        // calling function
+        const data = filteredData(req, 'name', 'email');
+
+
+        if (!data) return next(new errorHandling("Empty data", 404))
+        // update user with validation  
         const updateUser = await User.findByIdAndUpdate(userId, data, { new: true, runValidators: true });
 
-        if (!updateUser) return next(new errorHandling("User not found",404))
-            
+        if (!updateUser) return next(new errorHandling("User not found", 404))
 
-        res.status(200).json({ 
+        // send response
+        res.status(200).json({
             message: "Profile updated successfully",
-           
+
         });
     } catch (error) {
         next(error);
