b99ac978688c15ad32377c0f2ef085f67c611b26
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-04-01 13:59:29 +0545

    removing protect middleware

--- routing/tourRoute.js
+++ routing/tourRoute.js
@@ -4,7 +4,7 @@ const authController=require('../controller/authController')
 const router = express.Router()
 
 
-router.route('/').get(tours.getTours).post(authController.protect,authController.isAdmin,tours.postTour)
+router.route('/').get(tours.getTours).post(authController.protect,authController.isAdmin,tours.postTour);
 router.route('/:id').get(tours.getOnlyOneTour).patch(authController.protect,authController.isAdmin,tours.updateById).delete(authController.protect,authController.isAdmin,tours.deleteById)
 
 module.exports=router
