365944c2d1d9d575f09755a2bcc4cd632fa321a3
Ishwor-stha <ishwor.stha.bank@gmail.com>
2024-04-02 15:55:06 +0545

    protecting form nosql and xss attack

--- server.js
+++ server.js
@@ -1,98 +1,75 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-const rateLimit=require('express-rate-limit')
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const rateLimit = require('express-rate-limit');
 const mongoSanitize = require('express-mongo-sanitize');
+const { JSDOM } = require('jsdom');
+const createDOMPurify = require('dompurify');
+const { window } = new JSDOM('');
+const DOMPurify = createDOMPurify(window);
 
+const errorHandling = require('./util/errorHandling');
+const errorController = require('./controller/errorController');
+const tourRoute = require('./routing/tourRoute');
+const userRoute = require('./routing/userRoute');
 
-const errorHandling=require('./util/errorHandling')
-const errorController=require('./controller/errorController')
-const tourRoute=require('./routing/tourRoute')
-const userRoute=require('./routing/userRoute')
-
-
-/***********************************Limiting the request ****************************************************** */
-// limit the request from the Ip address
+/***********************************Limiting the request ******************************************************/
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Redis, Memcached, etc. See below.
-})
-// limit the request from the Ip address
-app.use(limiter)
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
+app.use(limiter);
 
- /*******Some configuration ********************************************************************************* */
- 
-
- // Parse incoming JSON request bodies
+/*******Some configuration ********************************************************************************* */
 app.use(express.json());
-// Configure dotenv to load environment variables from config.env file
 dotenv.config({ path: './config.env' });
 
-
-
 /************************Preventing from nosql injection and xss************************/
+app.use(mongoSanitize()); // Prevent NoSQL injection
 
-app.use(mongoSanitize());//calling function on middleware to prevent NOSql injection
-
-
-
+// Sanitize user input to prevent XSS attacks
+app.use((req, res, next) => {
+    // Check if request body exists and is an object
+    if (req.body && typeof req.body === 'object') {
+        // Iterate over each key in the request body
+        const arrayKey=Object.keys(req.body)
+        arrayKey.forEach(key => {
+            // Sanitize the value of each key using DOMPurify
+            req.body[key] = DOMPurify.sanitize(req.body[key]);
+        });
+    }
+    next();
+});
 
 /***********************************MongoDb connection**************************************************** */
-
-//Importing the mongodb connection string and replacing the <password> field from config.env file
 const db = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
-
-// making a async function to connect with database 
 async function connectMongodb() {
     try {
-
-        // connecting to db
         await mongoose.connect(db);
         console.log("Db connected");
-
     } catch (error) {
         console.error(`Error: ${error.message}`);
     }
-
 }
-// calling a function to connect with database
 connectMongodb();
 
-
 /****************************************ROUTE SECTION*********************************************** */
-
-
-// User section
-app.use('/api/v1/user',userRoute);
-// tours section
-app.use('/api/v1/tours',tourRoute);
-
-
+app.use('/api/v1/user', userRoute); // User section
+app.use('/api/v1/tours', tourRoute); // Tours section
 
 /*********************************Handling route error***************************************************** */
-
-
-
-// handling error if user enter other route than defined route
-// .all accepts all request method (get,post,patch,delete)and "*" accepts all route url
 app.all('*', (req, res, next) => {
-    // calling errorHandling classs while passing two arguments
-    next(new errorHandling(`The request url ${req.originalUrl} is not avaiable`,404));
-
-})
+    next(new errorHandling(`The request URL ${req.originalUrl} is not available`, 404));
+});
 
 /*********************************Error Handling middleware ***************************************************** */
-
-//redirecting to the error handling middleware  
 app.use(errorController);
 
-
-/********************************listening for  incomming request***************************************************/
-const port = process.env.PORT;//importing port from config.env file
+/********************************listening for incoming request***************************************************/
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log('Server started at port ' + port);
-})
+});
